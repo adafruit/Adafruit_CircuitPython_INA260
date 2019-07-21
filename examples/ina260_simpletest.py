2ecd6223e7a3bb07241a724871f2e886673ac120
siddacious <nospam187+github@gmail.com>
2019-07-21 15:31:45 -0700

    fixing typo in example

--- examples/ina260_simpletest.py
+++ examples/ina260_simpletest.py
@@ -5,6 +5,6 @@ import adafruit_ina260
 i2c = board.I2C()
 ina260 = adafruit_ina260.INA260(i2c)
 while True:
-    print("Current: %.2f mV Voltage: %.2f V Power:%.2f mW"
+    print("Current: %.2f mA Voltage: %.2f V Power:%.2f mW"
           %(ina260.current, ina260.voltage, ina260.power))
     time.sleep(1)
